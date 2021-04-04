0f2d56686029913e95a99b7576356296b5157df6
Abdul-Matheen2004 <81520758+Abdul-Matheen2004@users.noreply.github.com>
2021-04-04 10:26:09 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+function pressed() {
+    var Symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "_", "-"]
+    var input = document.getElementById("inp").value;
+    var teseter = input.split("")
+    if (input.length >= 8) {
+        document.getElementById("condition5").style.color = "green";
+    } else {
+        document.getElementById("condition5").style.color = "red";
+        document.getElementById("condition5").style.fontWeight = "1000";
+    }
+
+    for (i of input.split("")) {
+        if (Symbols.indexOf(i) !== -1) {
+            document.getElementById("condition3").style.color = "green";
+            document.getElementById("condition3").style.fontWeight = "1";
+            break;
+        } else {
+            document.getElementById("condition3").style.color = "red";
+            document.getElementById("condition3").style.fontWeight = "1000";
+        }
+
+    }
+    for (i of input.split("")) {
+        if (isNaN(i) == false) {
+            document.getElementById("condition4").style.color = "green";
+            document.getElementById("condition4").style.fontWeight = "1";
+            break;
+        } else {
+            document.getElementById("condition4").style.color = "red";
+            document.getElementById("condition4").style.fontWeight = "1000";
+        }
+    }
+    for (i of input.split("")) {
+        if (i === i.toUpperCase()) {
+            document.getElementById("condition1").style.color = "green";
+            document.getElementById("condition1").style.fontWeight = "1";
+            break;
+        } else {
+            document.getElementById("condition1").style.color = "red";
+            document.getElementById("condition1").style.fontWeight = "1000";
+        }
+    }
+    for (i of input.split("")) {
+        if (i === i.toLowerCase()) {
+            document.getElementById("condition2").style.color = "green";
+            document.getElementById("condition2").style.fontWeight = "1";
+            break;
+        } else {
+            document.getElementById("condition2").style.color = "red";
+            document.getElementById("condition2").style.fontWeight = "1000";
+        }
+    }
+}
